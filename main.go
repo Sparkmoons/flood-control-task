8a18cd764a294afbca2ae257266f53694ebf9dad
Sparkmoons <82277295+Sparkmoons@users.noreply.github.com>
2024-03-24 01:41:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,19 @@ import (
 )
 
 func main() {
+	//Адрес, пароль, временной промежуток, порог
+	fc := NewFloodControl("localhost:6379", "", 5*time.Second, 3)
 
+	userID := int64(159)
+	for i := 0; i < 5; i++ {
+		allowed, err := fc.Check(context.Background(), userID)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Printf("Check passed: %v\n", allowed)
+		time.Sleep(1 * time.Second)
+	}
 }
 
 // FloodControl интерфейс, который нужно реализовать.
